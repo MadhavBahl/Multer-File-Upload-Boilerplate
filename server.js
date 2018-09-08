1692871fbce724e638c2d4dbb9a6ca16ec759a07
Madhav Bahl <madhavbahl10@gmail.com>
2018-09-08 22:02:30 +0530

    Add POST /singleUpload route

--- server.js
+++ server.js
@@ -1,26 +1,59 @@
+/* =================================================== */
+/* ===== Section 1: Require all the dependencies ===== */
+/* =================================================== */
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const upload = multer({ dest: './uploads/' });
-var logger = require('morgan');
+const hbs = require('hbs');
+const logger = require('morgan');
 
+// Define port for app to listen on
 const port =  process.env.PORT || 8080;
 
-const app =  express();
-app.use(bodyParser());
-app.engine('html', require('ejs').renderFile);
-app.use(express.static(__dirname + '/public'));
-app.set('views', __dirname + '/views');
-app.set('view engine', 'html');
+/* ==================================================== */
+/* ===== Section 2: Configure express middlewares ===== */
+/* ==================================================== */
 
-app.use(logger('dev'));
+const app =  express();
+app.use(bodyParser());  // to use bodyParser (for text/number data transfer between clientg and server)
+app.set('view engine', 'hbs');  // setting hbs as the view engine
+app.use(express.static(__dirname + '/public'));  // making ./public as the static directory
+app.set('views', __dirname + '/views');  // making ./views as the views directory
+app.use(logger('dev'));  // Creating a logger (using morgan)
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+
+/* ==================================== */
+/* ===== Section 3: Making Routes ===== */
+/* ==================================== */
+
+// GET / route for serving index.html file
 app.get('/', (req, res) => {
-    res.render('index.html');
+    res.render('index.hbs');
 });
 
+// POST /singleUpload for single file upload
+/* ===== Make sure that file name matches the name attribute in your html ===== */
+app.post('/singleUpload', upload.single('myFile'), (req, res) => {
+    if (req.file) {
+        console.log('Uploading file...');
+        var filename = req.file.filename;
+        var uploadStatus = 'File Uploaded Successfully';
+    } else {
+        console.log('No File Uploaded');
+        var filename = 'FILE NOT UPLOADED';
+        var uploadStatus = 'File Upload Failed';
+    }
+    
+    /* ===== Add the function to save filename to database ===== */
+    
+    res.render('index.hbs', { status: uploadStatus, filename: `Name Of File: ${filename}` });
+});
+
+// To make the server live
 app.listen(port, () => {
     console.log(`App is live on port ${port}`);
 });
